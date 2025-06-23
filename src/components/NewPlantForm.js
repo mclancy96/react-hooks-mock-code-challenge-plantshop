2383616f51a022edbd5b1a36fdf777e73063ba27
Mike <mclancy96@gmail.com>
2025-06-23 14:39:21 -0400

    Add new plant with form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,67 @@
-import React from "react";
+import { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ setPlants }) {
+  const defaultState = {
+    name: "",
+    image: "",
+    price: 0,
+  };
+  const [plantData, setPlantData] = useState(defaultState);
+  const updatePlantData = (e) => {
+    setPlantData((currData) => {
+      const newData = { ...currData };
+      newData[e.target.name] =
+        e.target.name === "price"
+          ? Number.parseFloat(e.target.value)
+          : e.target.value;
+      return newData;
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      plantData &&
+      plantData.name.length > 0 &&
+      plantData.image.length > 0 &&
+      plantData.price >= 0
+    ) {
+      fetch("http://localhost:6001/plants", {
+        method: "POST",
+        body: JSON.stringify(plantData),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((r) => r.json())
+        .then((data) => setPlants((currPlants) => [...currPlants, data]));
+    } else {
+      alert("Missing information!");
+    }
+  };
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Plant name"
+          value={plantData.name}
+          onChange={updatePlantData}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={plantData.image}
+          onChange={updatePlantData}
+        />
+        <input
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+          value={plantData.price}
+          onChange={updatePlantData}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -7,9 +7,9 @@ function PlantCard({ plant, setPlants }) {
   };
   return (
     <li className="card">
-      <img src={plant.image} alt={"plant name"} />
+      <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
-      <p>Price: {plant.price}</p>
+      <p>Price: {plant.price.toFixed(2)}</p>
       {inStock ? (
         <button className="primary" onClick={toggleInStock}>
           In Stock
