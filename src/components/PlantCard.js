807be6189e4bce006ed65a0060cde1632d3d7c32
Mike <mclancy96@gmail.com>
2025-06-23 15:19:42 -0400

    Allow editing of price specifically

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,21 +2,61 @@ import { useState } from "react";
 
 function PlantCard({ plant, setPlants }) {
   const [inStock, setInStock] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [newPrice, setNewPrice] = useState(plant.price);
   const toggleInStock = () => {
     setInStock((curr) => !curr);
   };
+  const toggleEditing = () => {
+    setIsEditing((curr) => !curr);
+  };
+  const updateNewPrice = (e) => {
+    setNewPrice(parseFloat(e.target.value));
+  };
+
+  const handleSubmit = () => {
+    fetch(`http://localhost:6001/plants/${plant.id}`, {
+      method: "PATCH",
+      body: JSON.stringify({ price: parseFloat(newPrice) }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((r) => r.json())
+      .then((updatedPlant) => {
+        setPlants((plants) =>
+          plants.map((p) => (p.id === plant.id ? updatedPlant : p)),
+        );
+        toggleEditing();
+      });
+  };
+
+  const editPriceForm = (
+    <div>
+      <input
+        type="number"
+        onChange={updateNewPrice}
+        value={newPrice}
+        step={0.01}
+      />
+      <button onClick={handleSubmit}>Save</button>
+    </div>
+  );
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
-      <p>Price: {plant.price.toFixed(2)}</p>
-      {inStock ? (
-        <button className="primary" onClick={toggleInStock}>
-          In Stock
-        </button>
-      ) : (
-        <button onClick={toggleInStock}>Out of Stock</button>
-      )}
+      <div>
+        Price: {isEditing ? editPriceForm : <p>{plant.price.toFixed(2)}</p>}
+      </div>
+      <div>
+        {inStock ? (
+          <button className="primary" onClick={toggleInStock}>
+            In Stock
+          </button>
+        ) : (
+          <button onClick={toggleInStock}>Out of Stock</button>
+        )}
+        <button onClick={toggleEditing}>Edit Price</button>
+      </div>
     </li>
   );
 }
